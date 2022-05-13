df49fdbdff8a940ff5c5090f74ed560bf58f54df
t53657yn <yi.ngam@student.manchester.ac.uk>
2022-05-13 15:48:09 +0100

    Write Unit Tests for VenuesControllerTest
    Current Coverage for VenuesControllerTest: 100%

--- src/test/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerTest.java
@@ -4,13 +4,22 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Optional;
 
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -19,6 +28,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -59,6 +69,60 @@ public class VenuesControllerTest {
 		verify(venueService).findAll();
 		verifyNoInteractions(venue);
 	}
+	
+	@Test
+	public void getVenueUpcomingDate() throws Exception {
+		Venue v = new Venue();
+		v.setId(0);
+		v.setName("Kilburn Building");
+		v.setAddress("Kilburn Building University of Manchester, Oxford Rd, Manchester");
+		v.setPostalCode("M13 9PL");
+		v.setCapacity(200);
+		
+		Event e = new Event();
+		LocalDate d = LocalDate.now().plusDays(5);
+		LocalTime t = LocalTime.now();
+		
+		e.setId(0);
+		e.setName("Event");
+		e.setDescription("Description");
+		e.setDate(d);
+		e.setTime(t);
+		e.setVenue(v);
+		
+		when(venueService.findById(0)).thenReturn(Optional.of(v));
+		when(eventService.findAllByVenue(v)).thenReturn(Collections.<Event>singletonList(e));
+		
+		mvc.perform(get("/venues/0").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
+				.andExpect(view().name("venues/venue-information")).andExpect(handler().methodName("getVenue"));
+	}
+	
+	@Test
+	public void getVenueUpcomingTime() throws Exception {
+		Venue v = new Venue();
+		v.setId(0);
+		v.setName("Kilburn Building");
+		v.setAddress("Kilburn Building University of Manchester, Oxford Rd, Manchester");
+		v.setPostalCode("M13 9PL");
+		v.setCapacity(200);
+		
+		Event e = new Event();
+		LocalDate d = LocalDate.now();
+		LocalTime t = LocalTime.now().plusMinutes(5);
+		
+		e.setId(0);
+		e.setName("Event");
+		e.setDescription("Description");
+		e.setDate(d);
+		e.setTime(t);
+		e.setVenue(v);
+		
+		when(venueService.findById(0)).thenReturn(Optional.of(v));
+		when(eventService.findAllByVenue(v)).thenReturn(Collections.<Event>singletonList(e));
+		
+		mvc.perform(get("/venues/0").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
+				.andExpect(view().name("venues/venue-information")).andExpect(handler().methodName("getVenue"));
+	}
 
 	@Test
 	public void getVenueNotFound() throws Exception {
@@ -66,6 +130,62 @@ public class VenuesControllerTest {
 				.andExpect(view().name("venues/not_found")).andExpect(handler().methodName("getVenue"));
 	}
 	
+	@Test
+	@WithMockUser(username="Admin", roles= {"ADMINISTRATOR"})
+	public void getVenueCreate() throws Exception {
+		
+		when(venueService.findAll()).thenReturn(Collections.<Venue>emptyList());
+		
+		mvc.perform(get("/venues/venue_create").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
+				.andExpect(view().name("venues/venue_create")).andExpect(handler().methodName("createNewVenue"));
+	}
+	
+	@Test
+	@WithMockUser(username="Admin", roles= {"ADMINISTRATOR"})
+	public void postVenueCreate() throws Exception {
+		
+		when(venueService.findAll()).thenReturn(Collections.<Venue>emptyList());
+		
+		mvc.perform(post("/venues/venue_create").with(csrf())
+	            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+	            .content(EntityUtils.toString(new UrlEncodedFormEntity(Arrays.asList(
+	                    new BasicNameValuePair("name", "Kilburn Building"),
+	                    new BasicNameValuePair("address", "Kilburn Building University of Manchester, Oxford Rd, Manchester"),
+	                    new BasicNameValuePair("postalCode", "M13 9PL"),
+	                    new BasicNameValuePair("capacity", "200")
+	            )))))
+				.andExpect(view().name("venues/venue_create"))
+				.andExpect(status().isOk())
+				.andExpect(handler().methodName("createNewVenue"));
+	}
+	
+	@Test
+	@WithMockUser(username="Admin", roles= {"ADMINISTRATOR"})
+	public void getVenueUpdate() throws Exception {
+		
+		when(venueService.findAll()).thenReturn(Collections.<Venue>emptyList());
+		
+		mvc.perform(get("/venues/venue_update").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
+				.andExpect(view().name("venues/venue_update")).andExpect(handler().methodName("update"));
+	}
+	
+	@Test
+	@WithMockUser(username="Admin", roles= {"ADMINISTRATOR"})
+	public void postVenueUpdate() throws Exception {
+		
+		mvc.perform(post("/venues/venue_update").with(csrf())
+	            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+	            .content(EntityUtils.toString(new UrlEncodedFormEntity(Arrays.asList(
+	                    new BasicNameValuePair("id", "0"),
+	                    new BasicNameValuePair("name", "Kilburn Building"),
+	                    new BasicNameValuePair("address", "Kilburn Building University of Manchester, Oxford Rd, Manchester"),
+	                    new BasicNameValuePair("postalCode", "M13 9PL"),
+	                    new BasicNameValuePair("capacity", "200")
+	            )))))
+				.andExpect(view().name("venues/venue_update"))
+				.andExpect(status().isOk())
+				.andExpect(handler().methodName("update"));
+	}
 	
 	@Test
 	public void searchVenueNotFound() throws Exception{
